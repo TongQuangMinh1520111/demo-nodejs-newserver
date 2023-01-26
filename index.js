649c2021273b72f72026e50aea05b6265f0a0f65
TQMINHVN <tq-minh@b-create.co.jp>
2023-01-26 18:00:27 +0700

    fix

--- index.js
+++ index.js
@@ -4,9 +4,7 @@ var app = express();
 app.get('/', function (req, res) {
    res.send('Hello World');
 });
-
-var server = app.listen(3000, function () {
-  var host = server.address().address
-  var port = server.address().port
-  console.log("Ung dung Node.js dang hoat dong tai dia chi: http://%s:%s", host, port)
+const port = process.env.PORT || 3000;
+var server = app.listen(port, function () {
+  console.log("Ung dung Node.js dang hoat dong tai dia chi: http://%s:%s",)
 });
